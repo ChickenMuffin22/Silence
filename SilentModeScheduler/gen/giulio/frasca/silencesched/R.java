1e64d06a3b3d0985e7c8ba6f0b91f6850deeeafb
Spencer <smguy2@ncsu.edu>
2012-04-25 17:44:49 -0400

    updated icons

--- SilentModeScheduler/gen/giulio/frasca/silencesched/R.java
+++ SilentModeScheduler/gen/giulio/frasca/silencesched/R.java
@@ -18,8 +18,9 @@ public final class R {
     public static final class drawable {
         public static final int addicon=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int removeicon=0x7f020002;
-        public static final int visicon=0x7f020003;
+        public static final int ic_launcher_blackwhite=0x7f020002;
+        public static final int removeicon=0x7f020003;
+        public static final int visicon=0x7f020004;
     }
     public static final class id {
         public static final int LinearLayout01=0x7f070013;
