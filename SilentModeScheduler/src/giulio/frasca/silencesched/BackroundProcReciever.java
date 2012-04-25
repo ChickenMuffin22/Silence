abff7605a303d20031c6e84acd5e9f55a915907b
Giulio Frasca <gmfrasca@ncsu.edu>
2012-04-25 17:08:19 -0400

    Fixing Bootup Reciever

--- SilentModeScheduler/src/giulio/frasca/silencesched/BackroundProcReciever.java
+++ SilentModeScheduler/src/giulio/frasca/silencesched/BackroundProcReciever.java
@@ -6,22 +6,28 @@ import android.content.Intent;
 import android.util.Log;
 
 public class BackroundProcReciever extends BroadcastReceiver {
-	private boolean serviceRunning = true;
+	//private boolean serviceRunning = true;
     @Override
     public void onReceive(Context context, Intent intent) {
-        if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+    	Intent serviceIntent = new Intent(context, BackroundService.class);
+		context.startService(serviceIntent);
+//        if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
         	logcatPrint("SILENCESCHED STARTED");
-        	if (!serviceRunning){
-				serviceRunning=true;
-				context.startService(new Intent(BackroundService.class.getName()));
-				
-			}
-			else{
-				serviceRunning=false;
-				context.stopService(new Intent(BackroundService.class.getName()));
-			}
-            
-        }
+//        	if (!serviceRunning){
+//				serviceRunning=true;
+//				Intent serviceIntent = new Intent();
+//				serviceIntent.setAction("com.wissen.startatboot.MyService");
+//				context.startService(serviceIntent);
+//				//context.startService(new Intent(BackroundService.class.getName()));
+//				
+//			}
+//			else{
+//				//do nothing
+//				//serviceRunning=false;
+//				//context.stopService(new Intent(BackroundService.class.getName()));
+//			}
+//            
+//        }
     }
     
     public void logcatPrint(String message){
