e4b2941bfe1e4ff4e01a6de963bc12fd20b936c5
Spencer <smguy2@ncsu.edu>
2012-04-29 11:26:00 -0400

    added icons, updated comments on list activity

--- SilentModeScheduler/src/giulio/frasca/silencesched/ItemListActivity.java
+++ SilentModeScheduler/src/giulio/frasca/silencesched/ItemListActivity.java
@@ -1,5 +1,10 @@
 package giulio.frasca.silencesched;
 
+import giulio.frasca.lib.Formatter;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,6 +31,36 @@ public class ItemListActivity extends ListActivity {
 		setListAdapter(adapter);
 	}
 
+	
+	/**
+	 *  
+	 *  Code used for the spinner:
+	 *  
+    	ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item);
+    	adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    	LinkedList<RingerSettingBlock> list = schedule.getList();
+    	Iterator<RingerSettingBlock> i = list.iterator();
+    	nameDictionary.clear();
+    	nameDictionaryReverse.clear();
+    	boolean skippedFirst=true;
+    	int mapID=0;
+    	while (i.hasNext()){
+    		RingerSettingBlock thisBlock = i.next();
+    		if (skippedFirst && thisBlock.isEnabled() && thisBlock.getId()>=0){
+    			nameDictionary.put(mapID, thisBlock.getId());
+    			nameDictionaryReverse.put(thisBlock.getId(),mapID);
+    			adapter.add(Formatter.formatName(thisBlock));
+    			mapID++;
+    		}
+    		skippedFirst=true;
+    	}
+    	adapter.notifyDataSetChanged();
+    	alarmSpinner.setAdapter(adapter);
+    	adapter.notifyDataSetChanged();
+	 */
+	
+	
+	
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		String item = (String) getListAdapter().getItem(position);
